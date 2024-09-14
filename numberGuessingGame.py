d8b0c7adcb5af3486c0b6456b76358b9fb21b625
Tubagus Alta <altatubagus@gmail.com>
2024-09-14 22:27:51 +0700

    add minimum chance to guess

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,10 +1,12 @@
 # Idea project by geeksforgeeks.org
 import random
+import math
 
 
 def numberGuessingGameSystem():
-    print("\n=== Welcome To Number Guessing Game! ===")
     attemptCounter = 0
+    flag = 0
+    print("\n=== Welcome To Number Guessing Game! ===")
     while True:
         try:
             lowerLimit, upperLimit = map(
@@ -17,35 +19,42 @@ def numberGuessingGameSystem():
         except ValueError:
             print('Please input two numbers that separated by a " to "\n')
             continue
-        if lowerLimit >= 0:
+        if lowerLimit > 0:
             if upperLimit > lowerLimit:
-                print(
-                    f"\n--- Let's Play! ---\nThe range you choose is {lowerLimit} to {upperLimit}"
-                )
                 chosenRandomNumber = random.randint(lowerLimit, upperLimit)
+                totalChances = math.ceil(
+                    math.log(upperLimit - lowerLimit + 1) / math.log(2)
+                )
+                # totalChances = math.ceil(math.log2(upperLimit - lowerLimit + 1)
+                print(
+                    f"\n--- Let's Play! ---\nThe range you choose is {lowerLimit} to {upperLimit} and you have only {totalChances} chances to guess the number!"
+                )
                 break
             else:
                 print(
                     f"The upper limit cannot be {upperLimit} because the lower limit is greater than or equal to the upper limit!\n"
                 )
         else:
-            print("The range cannot be negative!\n")
-    while True:
+            print("The range must be greater than zero!\n")
+    while attemptCounter < totalChances:
         attemptCounter += 1
         userGuess = int(
             input(
-                f"\n!!! Attempts {attemptCounter} !!!\nInput your guess(only INTEGER): "
+                f"\n!!! Attempts {attemptCounter} !!!\nInput your guess (only INTEGER): "
             )
         )
         if userGuess == chosenRandomNumber:
             print(
                 f"CONGRATS!!! You have guessed it right, the chosen number is {chosenRandomNumber}, and you guessed it in {attemptCounter} attempts!!!\n"
             )
+            flag = 1
             break
-        elif userGuess > chosenRandomNumber:
+        elif userGuess > chosenRandomNumber and not (attemptCounter == totalChances):
             print("Try again! You guessed to high")
-        elif userGuess < chosenRandomNumber:
+        elif userGuess < chosenRandomNumber and not (attemptCounter == totalChances):
             print("Try again! You guessed to small")
+    if not flag:
+        print(f"\nThe number is {chosenRandomNumber}\nBetter luck next time!\n")
 
 
 def main():
